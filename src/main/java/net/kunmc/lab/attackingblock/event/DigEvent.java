c52d5c949be548ca53148a77002807e35b7387ab
dai-1103 <bosatsunumnum@gmail.com>
2021-11-24 20:43:58 +0900

    buc fix

--- src/main/java/net/kunmc/lab/attackingblock/event/DigEvent.java
+++ src/main/java/net/kunmc/lab/attackingblock/event/DigEvent.java
@@ -7,6 +7,7 @@ import com.comphenix.protocol.events.PacketEvent;
 import net.kunmc.lab.attackingblock.AttackingBlock;
 import net.kunmc.lab.attackingblock.blockmob.BlockMob;
 import net.kunmc.lab.attackingblock.blockmob.BlockMobList;
+import net.kunmc.lab.attackingblock.util.RandomCalculator;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.metadata.FixedMetadataValue;
@@ -17,42 +18,41 @@ public class DigEvent {
         ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(AttackingBlock.plugin, PacketType.Play.Client.BLOCK_DIG) {
             @Override
             public void onPacketReceiving(PacketEvent event) {
-                if (!AttackingBlock.isRunning) {
-                    return;
-                }
-
-                String digName = event.getPacket().getPlayerDigTypes().getValues().get(0).name();
-                if (!digName.equals("START_DESTROY_BLOCK")) {
-                    return;
-                }
-
-                Block targetBlock = event.getPlayer().rayTraceBlocks(4).getHitBlock();
-
-                if (targetBlock.getType() == Material.AIR) {
-                    return;
-                }
-                // すでにメタデータが入っているか確認
-                if (targetBlock.hasMetadata(Const.META_DATA_KEY)) {
-                    return;
-                }
-
                 try {
+                    if (!AttackingBlock.isRunning) {
+                        return;
+                    }
+
+                    String digName = event.getPacket().getPlayerDigTypes().getValues().get(0).name();
+                    if (!digName.equals("START_DESTROY_BLOCK")) {
+                        return;
+                    }
+
+                    Block targetBlock = event.getPlayer().rayTraceBlocks(4).getHitBlock();
+
+                    if (targetBlock.getType() == Material.AIR) {
+                        return;
+                    }
+                    // すでにメタデータが入っているか確認
+                    if (targetBlock.hasMetadata(Const.META_DATA_KEY)) {
+                        return;
+                    }
+
                     targetBlock.setMetadata(Const.META_DATA_KEY, new FixedMetadataValue(AttackingBlock.plugin, null));
+
+                    // 抽選
+                    if (!RandomCalculator.lottery(AttackingBlock.config.encounterProbability.value())) {
+                        return;
+                    }
+
+                    // エンティティを生み出す
+                    targetBlock.removeMetadata(Const.META_DATA_KEY, AttackingBlock.plugin);
+                    BlockMob blockMob = new BlockMob(targetBlock, event.getPlayer());
+                    BlockMobList.add(blockMob);
+
                 } catch (Exception ignored) {
 
                 }
-
-                // 抽選
-                Double num = Math.random();
-
-                if (num >= AttackingBlock.config.encounterProbability.value() / 100) {
-                    return;
-                }
-
-                // エンティティを生み出す
-                targetBlock.removeMetadata(Const.META_DATA_KEY, AttackingBlock.plugin);
-                BlockMob blockMob = new BlockMob(targetBlock, event.getPlayer());
-                BlockMobList.add(blockMob);
             }
         });
     }
